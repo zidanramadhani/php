fc1a942e355653c155a0a48e644cc30dc8258f50
zidanramadhani <76083041+zidanramadhani@users.noreply.github.com>
2021-04-08 10:39:14 +0800

    Add files via upload

--- variabel.php
+++ variabel.php
@@ -1,10 +1,24 @@
-<?php
-    $txt = "Hello world!";
-    $x = 5;
-    $y = 10.5;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Variabel</title>
+</head>
+<body>
+    <h2>Belajar Variabel</h2>
+    <p>
+        <?php
+
+            $nama = "Muhammad Zidan Ramadhani";
+            $semester = 4;
+            $nilai = 7.9;
+
+            echo "Nama Kamu ".$nama, " anda sedang di semester ".$semester. " dan nilai matkul web anda adalah ".
+            $nilai;
+
 ?>
-<?php
-    echo $txt."<br>";
-    echo $x."<br>";
-    echo $y."<br>";
-?>
+    
+    </p>
+</body>
+</html>
